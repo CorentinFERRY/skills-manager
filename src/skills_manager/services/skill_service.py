a000b8291519e28761c871c3b5f8b98d5674855a
Corentin FERRY <c.ferry78@laposte.net>
2026-04-01 14:57:50 +0200

    feat: add service for read and create Skill

---
+++ src/skills_manager/services/skill_service.py
@@ -0,0 +1,22 @@
+from typing import List
+
+import skills_manager.database.memory as memory
+from skills_manager.models.Skill import Skill
+
+
+def get_all_skills() -> List[Skill]:
+    return memory.skills
+
+
+def get_skill_by_id(skill_id: int) -> Skill | None:
+    for skill in memory.skills:
+        if skill.id == skill_id:
+            return skill
+    return None
+
+
+def create_skill(name: str) -> Skill:
+    new_skill = Skill(id=memory.current_skill_id, name=name)
+    memory.current_skill_id += 1
+    memory.skills.append(new_skill)
+    return new_skill
