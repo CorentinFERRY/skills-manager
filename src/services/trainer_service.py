5b37aa48126dd6fc8c6fbb1ad413ef580731af4a
Corentin <c.ferry78@laposte.net>
2026-04-03 12:19:56 +0200

    feat: update trainer_service for using repository

--- src/services/trainer_service.py
+++ src/services/trainer_service.py
@@ -1,38 +1,33 @@
 from typing import List
 
-import src.database.memory as memory
 from src.models.Trainer import Trainer
+from src.repositories import trainer_repository
 from src.schemas.trainer_schema import TrainerCreate
 
 
 def get_all_trainers() -> List[Trainer]:
-    return memory.trainers
+    trainers = trainer_repository.find_all()
+    return [Trainer(id=trainer["id"], name=trainer["name"]) for trainer in trainers]
 
 
 def get_trainer_by_id(trainer_id: int) -> Trainer | None:
-    for trainer in memory.trainers:
-        if trainer.id == trainer_id:
-            return trainer
-    return None
+    trainer = trainer_repository.find_by_id(trainer_id)
+    if not trainer:
+        return None
+    return Trainer(id=trainer["id"], name=trainer["name"])
 
 
 def create_trainer(name: str) -> Trainer:
-    new_trainer = Trainer(id=memory.current_trainer_id, name=name)
-    memory.current_trainer_id += 1
-    memory.trainers.append(new_trainer)
-    return new_trainer
+    trainer_id = trainer_repository.insert(name)
+    return Trainer(id=trainer_id, name=name)
 
 
 def update_trainer(trainer_id: int, data: TrainerCreate) -> Trainer | None:
-    for i, trainer in enumerate(memory.trainers):
-        if trainer.id == trainer_id:
-            trainer.name = data.name
-            return trainer
-    return None
+    update = trainer_repository.update(trainer_id, data.name)
+    if not update:
+        return None
+    return Trainer(id=trainer_id, name=data.name)
 
 
-def delete_trainer(trainer_id: int) -> Trainer | None:
-    for i, trainer in enumerate(memory.trainers):
-        if trainer.id == trainer_id:
-            return memory.trainers.pop(i)
-    return None
+def delete_trainer(trainer_id: int) -> bool:
+    return trainer_repository.delete(trainer_id)
