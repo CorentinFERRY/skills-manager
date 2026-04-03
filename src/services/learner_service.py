45c68b46fb957ab2fe54a11a16df4c29ef9bfd5a
Corentin <c.ferry78@laposte.net>
2026-04-03 10:37:05 +0200

    feat: update service for using repository

--- src/services/learner_service.py
+++ src/services/learner_service.py
@@ -1,38 +1,36 @@
 from typing import List
 
-import src.database.memory as memory
+from src.repositories import learner_repository
 from src.models.Learner import Learner
 from src.schemas.learner_schema import LearnerCreate
 
 
 def get_all_learners() -> List[Learner]:
-    return memory.learners
+    learners = learner_repository.find_all()
+    return [Learner(id = learner["id"], name = learner["name"]) for learner in learners]
 
 
 def get_learner_by_id(learner_id: int) -> Learner | None:
-    for learner in memory.learners:
-        if learner.id == learner_id:
-            return learner
-    return None
+    learner = learner_repository.find_by_id(learner_id)
+    if not learner:
+        return None
+    learner = Learner(id = learner["id"], name = learner["name"])
+    for skill in learner_repository.find_validated_skills(learner_id):
+        learner.add_validated_skill(skill["skill_id"])
+    return learner
 
 
 def create_learner(name: str) -> Learner:
-    new_learner = Learner(id=memory.current_learner_id, name=name)
-    memory.current_learner_id += 1
-    memory.learners.append(new_learner)
-    return new_learner
+    learner_id = learner_repository.insert(name)
+    return Learner(id=learner_id, name=name)
 
 
 def update_learner(learner_id: int, data: LearnerCreate) -> Learner | None:
-    for i, learner in enumerate(memory.learners):
-        if learner.id == learner_id:
-            learner.name = data.name
-            return learner
-    return None
+    updated = learner_repository.update(learner_id, data.name)
+    if not updated:
+        return None
+    return Learner(id=learner_id, name=data.name)
 
 
-def delete_learner(learner_id: int) -> Learner | None:
-    for i, learner in enumerate(memory.learners):
-        if learner.id == learner_id:
-            return memory.learners.pop(i)
-    return None
+def delete_learner(learner_id: int) -> bool:
+    return learner_repository.delete(learner_id)
